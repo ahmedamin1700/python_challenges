b6a19fa646fc1faffa028dd022e53bfade96ffad
Ahmed Amin <ahmedamin1700@gmail.com>
2020-11-10 21:53:20 +0200

    implement helpers functions for hours, minutes and days. 8 tests pass waiting for 4.

--- time_calculator/time_calculator.py
+++ time_calculator/time_calculator.py
@@ -21,11 +21,67 @@ def convert_start_to_minutes(time: str) -> int:
         return result
 
 
+def convert_duration_to_minutes(time: str) -> int:
+    """
+    Converts the duration to minutes.
+    :param time: str of the duration time.
+    :return: int calculated minutes.
+    """
+    return int(time.split(":")[0]) * 60 + int(time.split(":")[1])
+
+
+def calculate_hours(time):
+    hours = int(time / 60)
+    while hours > 24:
+        hours -= 24
+    return hours
+
+
+def calculate_minutes(time):
+    return time - (int(time / 60) * 60)
+
+
+def calculate_days(time):
+    return int(int(time / 60) / 24)
+
+
+def convert_to_twelve_format(time: int) -> str:
+    converted_message = ""
+    hours = calculate_hours(time)
+    minutes = calculate_minutes(time)
+    days = calculate_days(time)
+
+    if len(str(minutes)) < 2:
+        minutes_str = "0" + str(minutes)
+    else:
+        minutes_str = str(minutes)
+
+    if hours == 24:
+        converted_message = str(hours - 12) + ":" + minutes_str + " AM"
+    elif hours > 12:
+        converted_message = str(hours - 12) + ":" + minutes_str + " PM"
+    elif hours < 12:
+        converted_message = str(hours) + ":" + minutes_str + " AM"
+    elif hours == 12:
+        converted_message = str(hours) + ":" + minutes_str + " PM"
+    elif hours == 0:
+        converted_message = str(hours) + ":" + minutes_str + " AM"
+
+    if days > 1:
+        converted_message += " (" + str(days) + " days later)"
+    elif days > 0:
+        converted_message += " (next day)"
+
+    return converted_message
+
+
 def add_time(start, duration):
-    print(convert_start_to_minutes(start))
+    st = convert_start_to_minutes(start)
+    du = convert_duration_to_minutes(duration)
+    added = st + du
+    new_time = convert_to_twelve_format(added)
+    return new_time
 
 
-
-
-    # return new_time
-add_time("3:30 PM", "2:12")
+# print(add_time("11:40 AM", "28:00"))
+# add_time("3:30 PM", "2:12")
